9f0ce0e7cc3e006c5893f9762700ddd1e11e90d9
Adiv512 <104990371+Adiv512@users.noreply.github.com>
2023-04-06 08:31:38 +0530

    Add files via upload

---
+++ ResumeBuilder.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package resumebuilder;
+
+/**
+ *
+ * @author imadi
+ */
+public class ResumeBuilder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        loginPage a = new loginPage();
+        a.setVisible(true);
+    }
+    
+}
